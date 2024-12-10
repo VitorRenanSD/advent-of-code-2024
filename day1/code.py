6dde271ddc69b3c74d94bf980998739ce086146c
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2024-12-10 15:23:01 -0300

    Create code.py

---
+++ day1/code.py
@@ -0,0 +1,21 @@
+def calculaDistancia(caminhoArquivo):
+    listaEsquerda = []
+    listaDireita = []
+    
+    # Le o arquivo e separa as colunas em duas listas
+    with open(caminhoArquivo) as arquivo:
+        for linha in arquivo:
+            numeros = linha.split()
+            listaEsquerda.append(int(numeros[0]))
+            listaDireita.append(int(numeros[1]))
+    
+    # Ordena as listas
+    esquerdaOrdenada = sorted(listaEsquerda)
+    direitaOrdenada = sorted(listaDireita)
+    
+    # Calcula a soma das distancias
+    distanciaTotal = sum(abs(e - d) for e, d in zip(esquerdaOrdenada, direitaOrdenada))
+    
+    return distanciaTotal
+
+print(calculaDistancia(r'C:\Users\x\Desktop\AoC\day1input.txt'))
